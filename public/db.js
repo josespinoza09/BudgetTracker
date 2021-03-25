7356f22d39a1aae6d3d66bfb6a67349c6b55aabc
Jose Espinoza <jose20019004@gmail.com>
2021-03-25 13:12:48 -0400

    update db

--- public/db.js
+++ public/db.js
@@ -1,3 +1,10 @@
 let db;
 // create a new db for the database.
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    // create object store called "pending" and set autoIncrement to true
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
+  
