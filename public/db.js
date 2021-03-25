cac4c8f63feb8627e310feec2f84704acf79b26e
Jose Espinoza <jose20019004@gmail.com>
2021-03-25 13:15:41 -0400

    added save function

--- public/db.js
+++ public/db.js
@@ -20,3 +20,14 @@ request.onupgradeneeded = function (event) {
   request.onerror = function (event) {
     console.log("Error! " + event.target.errorCode);
   };
+
+  function saveRecord(save) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+  
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+  
+    // add record to your store with add method.
+    store.add(save);
+  }
