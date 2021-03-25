e763f2c6a11e848b264ae52b0a033d0647faceb1
Jose Espinoza <jose20019004@gmail.com>
2021-03-25 13:19:21 -0400

    update db

--- public/db.js
+++ public/db.js
@@ -1,45 +1,44 @@
 let db;
-// create a new db for the database.
+// create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    // create object store called "pending" and set autoIncrement to true
-    const db = event.target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
-  };
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
 
-  request.onsuccess = function (event) {
-    db = event.target.result;
-  
-    // check if app is online before reading from db
-    if (navigator.onLine) {
-      checkDatabase();
-    }
-  };
+request.onsuccess = function (event) {
+  db = event.target.result;
 
-  request.onerror = function (event) {
-    console.log("Error! " + event.target.errorCode);
-  };
-
-  function saveRecord(save) {
-    // create a transaction on the pending db with readwrite access
-    const transaction = db.transaction(["pending"], "readwrite");
-  
-    // access your pending object store
-    const store = transaction.objectStore("pending");
-  
-    // add record to your store with add method.
-    store.add(save);
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
   }
+};
 
-  function checkDatabase() {
-    // open a transaction on your pending db
-    const transaction = db.transaction(["pending"], "readwrite");
-    // access your pending object store
-    const store = transaction.objectStore("pending");
-    // get all records from store and set to a variable
-    const getAll = store.getAll();
-  }  
+request.onerror = function (event) {
+  console.log("ERROR! " + event.target.errorCode);
+};
+
+function saveRecord(save) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(save);
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
